f073700561f4fde1da43668d673cce378d4e3f6c
NotZeetaa <rodrigo2005contente@gmail.com>
2023-08-01 01:06:38 +0000

    techpack: audio: Silent more spam log

--- techpack/audio/dsp/mius/mius_mixer_controls.c
+++ techpack/audio/dsp/mius/mius_mixer_controls.c
@@ -568,14 +568,14 @@ int mius_ultrasound_tx_port_set(struct snd_kcontrol *kcontrol,
 	}
 
 	ultrasound_tx_port_cache = ucontrol->value.integer.value[0];
-	printk(KERN_DEBUG "[MIUS] Via ULTRASOUND_TX_PORT_ID enable=%d",
+	pr_debug("[MIUS] Via ULTRASOUND_TX_PORT_ID enable=%d",
 	       ultrasound_tx_port_cache);
 	if (ultrasound_tx_port_cache)
 		ret = mius_open_port(ULTRASOUND_TX_PORT_ID);
 	else
 		ret = mius_close_port(ULTRASOUND_TX_PORT_ID);
 
-	MI_PRINT_E("ultrasound_tx_port: enable=%d ret=%d",
+	MI_PRINT_D("ultrasound_tx_port: enable=%d ret=%d",
 		   ultrasound_tx_port_cache, ret);
 
 	return ret;
@@ -602,7 +602,7 @@ int mius_ultrasound_rx_port_set(struct snd_kcontrol *kcontrol,
 	}
 
 	ultrasound_rx_port_cache = ucontrol->value.integer.value[0];
-	printk(KERN_DEBUG
+	pr_debug(
 	       "[MIUS] if we are here, it is wrong for mius, enable=%d",
 	       ultrasound_rx_port_cache);
 	if (ultrasound_rx_port_cache)
@@ -610,7 +610,7 @@ int mius_ultrasound_rx_port_set(struct snd_kcontrol *kcontrol,
 	else
 		ret = mius_close_port(ULTRASOUND_RX_PORT_ID);
 
-	MI_PRINT_E("mi ultrasound_rx_port: enable=%d ret=%d",
+	MI_PRINT_D("mi ultrasound_rx_port: enable=%d ret=%d",
 		   ultrasound_rx_port_cache, ret);
 
 	return 0;
@@ -911,7 +911,7 @@ int mius_system_configuration_param_get(struct snd_kcontrol *kcontrol,
 
 	if (mc->shift >= MIUS_SYSTEM_CONFIGURATION_CUSTOM_SETTING_0 &&
 	    mc->shift <= MIUS_SYSTEM_CONFIGURATION_CUSTOM_SETTING_15) {
-		MI_PRINT_E("get MIUS_SYSTEM_CONFIGURATION_CUSTOM_SETTING_%02d",
+		MI_PRINT_D("get MIUS_SYSTEM_CONFIGURATION_CUSTOM_SETTING_%02d",
 			   mc->shift -
 				   MIUS_SYSTEM_CONFIGURATION_CUSTOM_SETTING_0);
 		ucontrol->value.integer.value[0] = 0;
@@ -1018,13 +1018,13 @@ int mius_system_configuration_param_put(struct snd_kcontrol *kcontrol,
 	    mc->shift <= MIUS_SYSTEM_CONFIGURATION_CUSTOM_SETTING_15) {
 		const size_t csi =
 			mc->shift - MIUS_SYSTEM_CONFIGURATION_CUSTOM_SETTING_0;
-		MI_PRINT_E(
+		MI_PRINT_D(
 			"MIUS_SYSTEM_CONFIGURATION_CUSTOM_SETTING_XX csi:%zu",
 			csi);
 		if (csi >=
 		    ARRAY_SIZE(mius_system_configuration_cache.custom_settings))
 			return -EINVAL;
-		MI_PRINT_E("ucontrol->value.integer.value[0]:%ld",
+		MI_PRINT_D("ucontrol->value.integer.value[0]:%ld",
 			   ucontrol->value.integer.value[0]);
 		mius_system_configuration_cache.custom_settings[csi] =
 			ucontrol->value.integer.value[0];
--- techpack/audio/dsp/q6afe.c
+++ techpack/audio/dsp/q6afe.c
@@ -1203,7 +1203,7 @@ static int32_t afe_callback(struct apr_client_data *data, void *priv)
 #ifdef CONFIG_US_PROXIMITY
 	} else if (data->opcode == MI_ULTRASOUND_OPCODE) {
 		if (NULL != data->payload) {
-			printk(KERN_DEBUG "[MIUS] mi ultrasound afe afe cb");
+			pr_debug("[MIUS] mi ultrasound afe afe cb");
 			mius_process_apr_payload(data->payload);
 		} else
 			pr_err("[EXPORT_SYMBOLLUS]: payload ptr is Invalid");
@@ -2450,11 +2450,8 @@ static void afe_send_custom_topology(void)
 	this_afe.set_custom_topology = 0;
 	cal_block = cal_utils_get_only_cal_block(this_afe.cal_data[cal_index]);
 	if (cal_block == NULL || cal_utils_is_cal_stale(cal_block)) {
-		pr_err("%s cal_block not found!!\n", __func__);
 		goto unlock;
 	}
-
-	pr_debug("%s: Sending cal_index cal %d\n", __func__, cal_index);
 
 	ret = remap_cal_data(cal_block, cal_index);
 	if (ret) {
@@ -3717,7 +3714,6 @@ static int send_afe_cal_type(int cal_index, int port_id)
 				this_afe.cal_data[cal_index]);
 
 	if (cal_block == NULL || cal_utils_is_cal_stale(cal_block)) {
-		pr_err_ratelimited("%s cal_block not found!!\n", __func__);
 		ret = -EINVAL;
 		goto unlock;
 	}
