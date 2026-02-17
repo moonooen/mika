00fb167fd3498504dec5137b80bba959efb11f91
munel <starscroch90@gmail.com>
2026-02-17 12:28:47 +0700

    sched/rt: Make update_curr_rt() more accurate rq->clock_task may be updated between the two calls of rq_clock_task() in update_curr_rt(). Calling rq_clock_task() only once makes it more accurate and efficient, taking update_curr() as reference.

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1038,8 +1038,8 @@ static void update_curr_rt(struct rq *rq)
 	struct task_struct *curr = rq->curr;
 	struct sched_rt_entity *rt_se = &curr->rt;
 	u64 delta_exec;
-	u64 now;
-
+        u64 now;
+	rq_clock_task(rq);
 	if (curr->sched_class != &rt_sched_class)
 		return;
 
