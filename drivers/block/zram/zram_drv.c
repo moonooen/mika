c07d19bfc1b0824a7125e608dabbe763f0caaac9
munel <starscroch90@gmail.com>
2026-02-24 18:08:44 +0700

    drivers: block: hardcode zram value to 8GB

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1845,7 +1845,7 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = memparse(buf, NULL);
+	disksize = 8192UL * 1024 * 1024;
 	if (!disksize)
 		return -EINVAL;
 
