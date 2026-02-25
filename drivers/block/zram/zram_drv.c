c27da5944ac799f1d062b075e620ad82dbd91a76
munel <starscroch90@gmail.com>
2026-02-25 15:52:03 +0700

    fixup! drivers: block: hardcode zram to 8GB this fixup commit https://github.com/moonooen/mika/commit/c07d19bfc1b0824a7125e608dabbe763f0caaac9

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1845,7 +1845,7 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = 8192UL * 1024 * 1024;
+	disksize = (u64)8192 * SZ_1M;
 	if (!disksize)
 		return -EINVAL;
 
